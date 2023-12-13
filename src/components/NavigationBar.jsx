836a25c00ac2610230019097cd62f7d304c748bb
sriparna-koar <koarsk03@gmail.com>
2023-12-13 16:15:45 +0530

    first commit

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,13 +1,9 @@
-// NavigationBar.js
-
 import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import './NavigationBar.css';
+import './NavigationBar.css'; 
 
 const NavigationBar = () => {
   const [isSticky, setIsSticky] = useState(false);
-  const [isMobile, setIsMobile] = useState(false);
-  const [isMenuOpen, setIsMenuOpen] = useState(false); // State to manage mobile menu visibility
 
   useEffect(() => {
     const handleScroll = () => {
@@ -21,37 +17,14 @@ const NavigationBar = () => {
 
     window.addEventListener('scroll', handleScroll);
 
+    // Clean up the event listener
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth <= 768) {
-        setIsMobile(true);
-      } else {
-        setIsMobile(false);
-      }
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
-  const closeMenu = () => {
-    setIsMenuOpen(false);
-  };
-
   const navLinks = [
-    { to: '/', text: 'Home' },
+    {to:'/', text:'Home'},
     { to: '/introduction', text: 'Introduction' },
     { to: '/projects', text: 'Projects' },
     { to: '/skills', text: 'Skills' },
@@ -61,33 +34,16 @@ const NavigationBar = () => {
   ];
 
   return (
-    <nav className={`${isSticky ? 'navbar sticky' : 'navbar'} ${isMobile ? 'mobile' : ''}`}>
-      {isMobile ? (
-        <div className="mobile-menu-icon" onClick={toggleMenu}>
-          <i className={`fa ${isMenuOpen ? 'fa-times' : 'fa-bars'}`}></i>
-        </div>
-      ) : (
-        <ul className="nav-links">
-          {navLinks.map((link, index) => (
-            <li key={index}>
-              <NavLink to={link.to} activeClassName="active" onClick={closeMenu}>
-                {link.text}
-              </NavLink>
-            </li>
-          ))}
-        </ul>
-      )}
-      {isMobile && isMenuOpen && (
-        <ul className="mobile-nav-links">
-          {navLinks.map((link, index) => (
-            <li key={index}>
-              <NavLink to={link.to} activeClassName="active" onClick={closeMenu}>
-                {link.text}
-              </NavLink>
-            </li>
-          ))}
-        </ul>
-      )}
+    <nav className={isSticky ? "navbar sticky" : "navbar"}>
+      <ul className="nav-links">
+        {navLinks.map((link, index) => (
+          <li key={index}>
+            <NavLink to={link.to} activeClassName="active">
+              {link.text}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 };
