00c6609d90d28d4029718a2c93e00fd2e32d671c
sriparna-koar <koarsk03@gmail.com>
2023-12-16 11:59:38 +0530

    first commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,25 @@
+// import React from 'react';
+// import NavigationBar from './NavigationBar';
+// import Footer from './Footer';
+// import './Home.css';
+
+// const Home = () => {
+//   return (
+//     <section className="intro-section">
+//       <div className="home-container">
+//         <div className="home-overlay"></div>
+//         <h1>Welcome to My Portfolio</h1>
+//         <NavigationBar />
+//         <p className="intro-text">Hi, I'm Sriparna Koar, a passionate UI & UX designer and web developer. I am dedicated to creating user-friendly and visually appealing experiences.</p>
+//         <Footer/>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Home;
 import React from 'react';
+import { motion } from 'framer-motion';
 import NavigationBar from './NavigationBar';
 import Footer from './Footer';
 import './Home.css';
@@ -8,37 +29,29 @@ const Home = () => {
     <section className="intro-section">
       <div className="home-container">
         <div className="home-overlay"></div>
-        <h1>Welcome to My Portfolio</h1>
+        <motion.h1
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
+          whileHover={{ scale: 1.1, color: '#ff7f50', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)' }}
+          whileTap={{ scale: 0.9 }}
+        >
+          Welcome to My Portfolio
+        </motion.h1>
         <NavigationBar />
-        <p className="intro-text">Hi, I'm Sriparna Koar, a passionate UI & UX designer and web developer. I am dedicated to creating user-friendly and visually appealing experiences.</p>
-        <Footer/>
+        <motion.p
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 0.5 }}
+          className="intro-text"
+          whileHover={{ scale: 1.05 }}
+        >
+          Hi, I'm Sriparna Koar, a passionate UI & UX designer and web developer. I am dedicated to creating user-friendly and visually appealing experiences.
+        </motion.p>
+        <Footer />
       </div>
     </section>
   );
 };
 
 export default Home;
-
-{/* 
-import React from 'react';
-import './Introduction.css';
-import Footer from './Footer';
-import NavigationBar from './NavigationBar';
-
-const Introduction = () => {
-  return (
-    
-    <section className="intro-section">
-       <NavigationBar />
-     
-      <p className="intro-text">Hi, I'm Sriparna Koar, a passionate UI & UX designer and web developer. I am dedicated to creating user-friendly and
-                    visually appealing experiences. With a background in BTECH CSE-AI from Techno India University, I
-                    bring a unique blend of technical skills and design aesthetics to my projects</p>
-    
-                    <Footer/>
-    </section>
-    
-  );
-};
-
-export default Introduction; */}
--- src/components/qualifications.jsx
+++ src/components/qualifications.jsx
@@ -1,43 +1,88 @@
+// import React from 'react';
+// import NavigationBar from './NavigationBar';
+// import Footer from './Footer';
+// import './qualifications.css';
+// const Education = () => {
+ 
+//   const qualifications = [
+//     {
+//       degree: 'Bachelor of Science in Computer Science',
+//       institution: 'University Name',
+//       location: 'City, Country',
+//       year: '2018 - 2022',
+//       description: 'Description or additional details (optional)',
+//     },
+//     {
+//       degree: 'Master of Business Administration',
+//       institution: 'Another University',
+//       location: 'Another City, Country',
+//       year: '2023 - Present',
+//       description: 'Additional degree or relevant education (optional)',
+//     },
+
+//   ];
+
+//   return (
+//     <section className="education-section">
+//        <NavigationBar />
+//       <h1>Educational Qualifications</h1>
+//       <div className="qualifications-list">
+//         {qualifications.map((qualification, index) => (
+//           <div key={index} className="qualification-item">
+//             <h3>{qualification.degree}</h3>
+//             <p>{qualification.institution}</p>
+//             <p>{qualification.location}</p>
+//             <p>{qualification.year}</p>
+//             {qualification.description && <p>{qualification.description}</p>}
+//           </div>
+//         ))}
+//       </div>
+//       <Footer/>
+      
+//     </section>
+//   );
+// };
+
+// export default Education;
 import React from 'react';
 import NavigationBar from './NavigationBar';
 import Footer from './Footer';
+import './qualifications.css';
+
 const Education = () => {
- 
   const qualifications = [
     {
       degree: 'Bachelor of Science in Computer Science',
-      institution: 'University Name',
-      location: 'City, Country',
-      year: '2018 - 2022',
-      description: 'Description or additional details (optional)',
+      institution: 'Techno India University',
+      location: 'West Bengal,Kolkata',
+      year: '2022 - 2026',
+      description: 'Persuing Btech CSE_AI from TIU ',
     },
     {
-      degree: 'Master of Business Administration',
-      institution: 'Another University',
-      location: 'Another City, Country',
-      year: '2023 - Present',
-      description: 'Additional degree or relevant education (optional)',
+      degree: 'Higher Secondary',
+      School: 'Delhi Public School,Ruby Park',
+      location: 'West Bengal, Kolkata',
+      year: '2020 - 2022',
+      description: 'Persued class 12 from DPS,Ruby Park with core subjects Math,Physics,Chemistry,Computer Science',
     },
-    // Add more educational qualifications as needed
   ];
 
   return (
     <section className="education-section">
-       <NavigationBar />
-      <h1>Educational Qualifications</h1>
+      <NavigationBar />
+      <h1 className="education-heading">Educational Qualifications</h1>
       <div className="qualifications-list">
         {qualifications.map((qualification, index) => (
           <div key={index} className="qualification-item">
-            <h3>{qualification.degree}</h3>
-            <p>{qualification.institution}</p>
-            <p>{qualification.location}</p>
-            <p>{qualification.year}</p>
-            {qualification.description && <p>{qualification.description}</p>}
+            <h3 className="degree">{qualification.degree}</h3>
+            <p className="institution">{qualification.institution}</p>
+            <p className="location">{qualification.location}</p>
+            <p className="year">{qualification.year}</p>
+            {qualification.description && <p className="description">{qualification.description}</p>}
           </div>
         ))}
       </div>
-      <Footer/>
-      
+      <Footer />
     </section>
   );
 };
